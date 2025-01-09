2ca086cdd26c25ba8a1a71c40037d1e8503b9483
Jack299q <1625621154@qq.com>
2025-01-09 10:08:09 +0800

    项目初始化

---
+++ src/main/java/AgriRecommend/AgriRecommendApplication.java
@@ -0,0 +1,13 @@
+package AgriRecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriRecommendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/AgriRecommend/AgriRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package AgriRecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriRecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
